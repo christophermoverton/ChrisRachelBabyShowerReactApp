29fc8fc77db040cb9fffad8f4efb37c784c2a8b5
Christopher Overton <christophermoverton@gmail.com>
2017-11-07 23:42:51 -0600

    save

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Backgroundartcomp from './Backgroundartcomponent';
-import * as d3 from 'd3';
+
+import { BrowserRouter } from 'react-router-dom'
+
 
 function Square(props) {
     return (
@@ -132,133 +133,18 @@ function Square(props) {
     }
   }
   
-  class Game extends React.Component {
-    componentDidMount() {
-      this.transition();
-    }
-    transition() {
-      var oldText = [];
-      oldText.push(d3.select('#T1').select('text').text());
-      oldText.push(d3.select('#T2').select('text').text());
-      oldText.push(d3.select('#T3').select('text').text());
-      oldText.push(d3.select('#T4').select('text').text());
-      oldText.push(d3.select('#T5').select('text').text());
-      oldText.push(d3.select('#T6').select('text').text());
-      oldText.push(d3.select('#T7').select('text').text());
-      console.log(oldText);
-      var elements = ["#T1","#T2","#T3","#T4","#T5"];
-      
-      d3.select("#T1").selectAll('text').transition()
-          .duration(4000).delay(17000).ease(d3.easeLinear).tween("text", function () {
-            
-              var newText = oldText[0];
-              console.log(newText);
-              console.log(0);
-              console.log(oldText[0]);
-              var textLength = newText.length;
-              return function (t) {
-                d3.select("#T1").selectAll('text').text(newText.slice(0, 
-                                     Math.round( t * textLength) ));
-              };
-          });
 
-          d3.select("#T2").selectAll('text').transition()
-          .duration(4000).delay(22000).ease(d3.easeLinear).tween("text", function () {
-            
-              var newText = oldText[1];
-              console.log(newText);
-              console.log(0);
-              console.log(oldText[1]);
-              var textLength = newText.length;
-              return function (t) {
-                d3.select("#T2").selectAll('text').text(newText.slice(0, 
-                                     Math.round( t * textLength) ));
-              };
-          });
-
-          d3.select("#T3").selectAll('#t3a').transition()
-          .duration(4000).delay(27000).ease(d3.easeLinear).tween("tspan", function () {
-            
-              var newText = oldText[2];
-              console.log(newText);
-              console.log(0);
-              console.log(oldText[2]);
-              var textLength = newText.length;
-              return function (t) {
-                d3.select("#T3").selectAll('#t3a').text(newText.slice(0, 
-                                     Math.round( t * textLength) ));
-              };
-          });
-
-        
-          d3.select("#T4").selectAll('#t4a').transition()
-          .duration(4000).delay(32000).ease(d3.easeLinear).tween("tspan", function () {
-            
-              var newText = oldText[3];
-              console.log(newText);
-              console.log(oldText[3]);
-              var textLength = newText.length;
-              return function (t) {
-                d3.select("#T4").selectAll('#t4a').text(newText.slice(0, 
-                                     Math.round( t * textLength) ));
-              };
-          });
-
-          d3.select("#T5").selectAll('#t5a').transition()
-          .duration(4000).delay(37000).ease(d3.easeLinear).tween("tspan", function () {
-            
-              var newText = oldText[4];
-              console.log(newText);
-              console.log(oldText[4]);
-              var textLength = newText.length;
-              return function (t) {
-                d3.select("#T5").selectAll('#t5a').text(newText.slice(0, 
-                                     Math.round( t * textLength) ));
-              };
-          });
-
-          d3.select("#T6").selectAll('#t6a').transition()
-          .duration(4000).delay(42000).ease(d3.easeLinear).tween("tspan", function () {
-            
-              var newText = oldText[5];
-              console.log(newText);
-              console.log(oldText[5]);
-              var textLength = newText.length;
-              return function (t) {
-                d3.select("#T6").selectAll('#t6a').text(newText.slice(0, 
-                                     Math.round( t * textLength) ));
-              };
-          });
-
-          d3.select("#T7").selectAll('#t7a').transition()
-          .duration(4000).delay(32000).ease(d3.easeLinear).tween("tspan", function () {
-            
-              var newText = oldText[6];
-              console.log(newText);
-              console.log(oldText[6]);
-              var textLength = newText.length;
-              return function (t) {
-                d3.select("#T7").selectAll('#t7a').text(newText.slice(0, 
-                                     Math.round( t * textLength) ));
-              };
-          });
-    }
- 
-    render() {
-      return (
-        <div className="game">
-
-
-        <Backgroundartcomp/>
-        </div>
-        
-      );
-    }
-  }
   
   // ========================================
   
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+
+
+  // main.js
+
+
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+), document.getElementById('root'))
